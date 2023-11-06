62ac8a0644864fe1becb81c6ecbf948a0733f332
Nolan Lewandowski <122854016+NLewandowski5@users.noreply.github.com>
2023-11-06 10:55:06 -0600

    Worked on hamburger menu
    worked on hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const hamburgerIcon = document.querySelector('.hamburger-menu');
-
-hamburgerIcon.addEventListener('click', () => {
-    mobileMenu.classList.toggle('show-mobile-menu');
-});
+//Hamburger menu function
+function hamburger(){
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("logo");
+  
+    if(menu.style.display === "block" && logo.style.display === "none"){
+      menu.style.display = "none";
+      logo.style.display = "block";
+    } else{
+      menu.style.display = "block";
+      logo.style.display = "none";
+    }
+  }
